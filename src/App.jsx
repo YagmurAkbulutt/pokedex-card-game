6933deec90a6bb27045647617d3149d28979f921
YagmurAkbulutt <yagmurakbulutt21@gmail.com>
2024-04-24 11:55:16 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+
+import './App.css';
+import './Pokecard.css'
+import Pokedex from './Pokedex';
+import Pokegame from './Pokegame';
+
+
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Pokemon Go Card Game</h1>
+      <Pokegame/>
+     
+    </div>
+  );
+}
+
+export default App;
